6679c6215090e8dd9b793a17e2deefeed20b1000
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-12-13 22:28:11 +0600

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Цырендылыкова Эржена on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
